cb2f57280db024a83a4e9a0894e6c1a382a2b8eb
Edmundo Paulino <manyymoore@gmail.com>
2020-09-06 15:24:21 -0300

    Default permission

--- src/database/migrations/00-create-users.js
+++ src/database/migrations/00-create-users.js
@@ -4,9 +4,9 @@ exports.up = (knex) =>
     table.string('email').unique().notNullable();
     table.string('password');
     // check if the user is an admin
-    table.boolean('isAdmin').notNullable();
+    table.boolean('isAdmin').notNullable().defaultTo(false);
     // check if the user is a moderator
-    table.boolean('isMod').notNullable();
+    table.boolean('isMod').notNullable().defaultTo(false);
     // the inserter or the updater of this register
     table.string('upserter').notNullable().defaultTo('anonymous');
     table.timestamps();
