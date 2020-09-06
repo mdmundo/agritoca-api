fac26653a247e0bff1a2e043c5ae7dec45c129bf
Edmundo Paulino <manyymoore@gmail.com>
2020-09-06 16:57:52 -0300

    snake, seed

--- src/database/seeds/02-products.js
+++ src/database/seeds/02-products.js
@@ -6,124 +6,124 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('products').insert([
         {
-          NCM: 7384,
+          ncm: 7384,
           measure: 'm2',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 6022,
+          ncm: 6022,
           measure: 'cm2',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 6949,
+          ncm: 6949,
           measure: 'm',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 9473,
+          ncm: 9473,
           measure: 'cm',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 8857,
+          ncm: 8857,
           measure: 'UN',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 5474,
+          ncm: 5474,
           measure: 'CT',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 7432,
+          ncm: 7432,
           measure: 'CX',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 3592,
+          ncm: 3592,
           measure: 'DZ',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 7927,
+          ncm: 7927,
           measure: 'GS',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 3555,
+          ncm: 3555,
           measure: 'PA',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 2822,
+          ncm: 2822,
           measure: 'PÇ',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 8663,
+          ncm: 8663,
           measure: 'PR',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 1394,
+          ncm: 1394,
           measure: 'PT',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 1661,
+          ncm: 1661,
           measure: 'RL',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 8209,
+          ncm: 8209,
           measure: 'kg',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 1964,
+          ncm: 1964,
           measure: 'g',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 6552,
+          ncm: 6552,
           measure: 'SC60',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 5714,
+          ncm: 5714,
           measure: 'l',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 6120,
+          ncm: 6120,
           measure: 'm3',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         },
         {
-          NCM: 2914,
+          ncm: 2914,
           measure: 'ml',
           description: 'a nice product description',
-          isOrganic: false
+          is_organic: false
         }
       ]);
     });
