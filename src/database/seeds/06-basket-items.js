16d9b1d7a13728dc9e7f7cdd811c485768fe4f9f
Edmundo Paulino <manyymoore@gmail.com>
2020-09-06 17:24:21 -0300

    seeds complete?

---
+++ src/database/seeds/06-basket-items.js
@@ -0,0 +1,30 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('basket_items')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('basket_items').insert([
+        { basket_id: 1, producer_product_id: 1 },
+        { basket_id: 1, producer_product_id: 2 },
+        { basket_id: 1, producer_product_id: 3 },
+        { basket_id: 1, producer_product_id: 4 },
+        { basket_id: 1, producer_product_id: 5 },
+        { basket_id: 1, producer_product_id: 6 },
+        { basket_id: 1, producer_product_id: 7 },
+        { basket_id: 1, producer_product_id: 8 },
+        { basket_id: 1, producer_product_id: 9 },
+        { basket_id: 1, producer_product_id: 10 },
+        { basket_id: 1, producer_product_id: 11 },
+        { basket_id: 2, producer_product_id: 12 },
+        { basket_id: 3, producer_product_id: 13 },
+        { basket_id: 4, producer_product_id: 14 },
+        { basket_id: 5, producer_product_id: 15 },
+        { basket_id: 6, producer_product_id: 16 },
+        { basket_id: 7, producer_product_id: 17 },
+        { basket_id: 8, producer_product_id: 18 },
+        { basket_id: 9, producer_product_id: 19 },
+        { basket_id: 10, producer_product_id: 20 }
+      ]);
+    });
+};
