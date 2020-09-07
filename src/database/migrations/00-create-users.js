f96a25fa5db0993dce9c573ff79bd991ecc5099e
Edmundo Paulino <manyymoore@gmail.com>
2020-09-07 11:29:20 -0300

    remove upserter from user, password default null

--- src/database/migrations/00-create-users.js
+++ src/database/migrations/00-create-users.js
@@ -2,13 +2,11 @@ exports.up = (knex) =>
   knex.schema.createTable('users', (table) => {
     table.increments().primary().index();
     table.string('email').unique().notNullable();
-    table.string('password');
+    table.string('password').defaultTo(null);
     // check if the user is an admin
     table.boolean('is_admin').notNullable().defaultTo(false);
     // check if the user is a moderator
     table.boolean('is_mod').notNullable().defaultTo(false);
-    // the inserter or the updater of this register
-    table.string('upserter').notNullable().defaultTo('anonymous');
     table.timestamps(true, true);
   });
 
