f5172916063b53facdadad49e10d0b2b79e9ae5e
Edmundo Paulino <manyymoore@gmail.com>
2020-09-07 10:51:58 -0300

    check admin privilege

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,14 +5,16 @@ const knex = require('../database/connection');
 const userController = {
   async all(req, res) {
     // check if is admin
+    if (req.user.is_admin) {
+      try {
+        const users = await knex('users').select('*');
 
-    try {
-      const users = await knex('users').select('*');
-
-      return res.json(users);
-    } catch (error) {
-      return res.status(500).json({ message: 'Error on Server', error });
+        return res.json(users);
+      } catch (error) {
+        return res.status(500).json({ message: 'Error on Server', error });
+      }
     }
+    return res.status(401).send({ message: 'You are nothing' });
   },
   async self(req, res) {},
   async create(req, res) {},
