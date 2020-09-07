b658140c1d52b69d303d804dee4042d25ff2bb74
Edmundo Paulino <manyymoore@gmail.com>
2020-09-07 17:59:09 -0300

    install multer and sharp

---
+++ src/utils/multer.js
@@ -0,0 +1,12 @@
+module.exports = {
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image.'));
+    }
+
+    cb(undefined, true);
+  }
+};
