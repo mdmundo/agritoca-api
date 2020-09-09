15a780a0e6b8a33bbaf64dab174e5c8d6362025d
Edmundo Paulino <manyymoore@gmail.com>
2020-09-09 15:14:47 -0300

    Update user tests

--- test/user.test.js
+++ test/user.test.js
@@ -25,6 +25,38 @@ test('Should not create existing user', async () => {
     .expect(400);
 });
 
+test('Should not create user with invalid email', async () => {
+  const response = await request(app)
+    .post('/users')
+    .send({
+      email: 'not_a_valid_at_sign_email_dot_com',
+      password: 'Qqk}X%CPuDte5jw]'
+    })
+    .expect(400);
+});
+
+test('Should not create user with short password', async () => {
+  const response = await request(app)
+    .post('/users')
+    .send({
+      email: 'newuser123@msn.com',
+      password: '1234567'
+    })
+    .expect(400);
+});
+
+test('Should not create user with invalid fields', async () => {
+  const response = await request(app)
+    .post('/users')
+    .send({
+      email: 'newuser123@msn.com',
+      password: 'Qqk}X%CPuDte5jw]',
+      is_admin: true,
+      is_mod: true
+    })
+    .expect(400);
+});
+
 describe('Require authentication', () => {
   beforeEach(setupAuth);
 
@@ -34,13 +66,49 @@ describe('Require authentication', () => {
       .set('Authorization', `Bearer ${users[0].token}`)
       .send()
       .expect(200);
+
+    const user = await knex('users').where('id', '=', users[0].id).first();
+    expect(user.is_admin).toBe(true);
   });
 
-  test('Should not fetch all users (mod)', async () => {
+  test('Should not fetch all users (not admin)', async () => {
     const response = await request(app)
       .get('/users')
       .set('Authorization', `Bearer ${users[1].token}`)
       .send()
       .expect(401);
+
+    const user = await knex('users').where('id', '=', users[1].id).first();
+    expect(user.is_admin).toBe(false);
+  });
+
+  test('Should fetch current user', async () => {
+    const response = await request(app)
+      .get('/users/me')
+      .set('Authorization', `Bearer ${users[0].token}`)
+      .send()
+      .expect(200);
+  });
+
+  test('Should set admin privilege', async () => {
+    const response = await request(app)
+      .post('/users/set/admin')
+      .set('Authorization', `Bearer ${users[0].token}`)
+      .send({ id: users[1].id })
+      .expect(200);
+
+    const user = await knex('users').where('id', '=', users[1].id).first();
+    expect(user.is_admin).toBe(true);
+  });
+
+  test('Should unset mod privilege', async () => {
+    const response = await request(app)
+      .post('/users/unset/mod')
+      .set('Authorization', `Bearer ${users[0].token}`)
+      .send({ id: users[1].id })
+      .expect(200);
+
+    const user = await knex('users').where('id', '=', users[1].id).first();
+    expect(user.is_mod).toBe(false);
   });
 });
