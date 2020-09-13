e6f0cae3b50398e5cfc891a9b19fe52e00dd74c8
Edmundo Paulino <manyymoore@gmail.com>
2020-09-12 21:32:59 -0300

    add default avatar to the game

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { errors } = require('celebrate');
+const path = require('path');
 const routers = require('./routers');
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(routers);
 app.use(errors());
