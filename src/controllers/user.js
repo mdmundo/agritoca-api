e102141d6455fee63006484d39ba6fda7d00f9d6
Edmundo Paulino <manyymoore@gmail.com>
2020-09-16 15:57:30 -0300

    user controller refac, user resource added.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const axios = require('axios');
 const knex = require('../../database/connection');
 const { getUserWithoutPassword } = require('../utils/public');
+const userResource = require('../resources/user');
 
 const userController = {
   async read(req, res) {
@@ -9,15 +10,14 @@ const userController = {
 
     try {
       if (req.query.id) {
-        const [user] = await knex('users').where({ id: req.query.id });
+        const user = await userResource.getUserById(req.query.id);
 
         return res.json(getUserWithoutPassword(user));
       }
 
-      const users = await knex('users').orderBy('id');
+      const users = await userResource.getAllUsers();
 
       const serializedUsers = users.map((user) => getUserWithoutPassword(user));
-
       return res.json(serializedUsers);
     } catch (error) {
       return res.status(500).json({ message: 'Error on Server', error });
@@ -28,39 +28,43 @@ const userController = {
       // is Google authentication?
       if (req.body.tokenId) {
         const {
-          data: { name, email, picture }
-        } = await axios.get(
-          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${req.body.tokenId}`
-        );
+          name,
+          email,
+          picture
+        } = await userResource.getGoogleUserProfile(req.body.tokenId);
 
         // is the user already on db?
-        const [user] = await knex('users')
-          .where({ email })
-          .first()
-          .update({ name, picture, updated_at: knex.fn.now() })
-          .returning('*');
-        if (user) {
-          const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-          return res.json({ user: getUserWithoutPassword(user), token });
+        {
+          const user = await userResource.getUpdatedUser({
+            name,
+            email,
+            picture
+          });
+          if (user) {
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+            return res.json({ user: getUserWithoutPassword(user), token });
+          }
         }
 
         // Create a new user
-        const [newUser] = await knex('users')
-          .insert({ name, email, picture })
-          .returning('*');
-        const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET);
+        const user = await userResource.getInsertedUser({
+          name,
+          email,
+          picture
+        });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
         return res
           .status(201)
-          .json({ user: getUserWithoutPassword(newUser), token });
+          .json({ user: getUserWithoutPassword(user), token });
       }
       // Create a new anonymous user
-      const [user] = await knex('users').insert({}).returning('*');
+      const user = await userResource.getInsertedUser({});
       const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
       return res
         .status(201)
         .json({ user: getUserWithoutPassword(user), token });
     } catch (error) {
-      return res.status(500).json({ message: 'Error on Server', error });
+      return res.status(500).json({ message: 'Error on Signing User', error });
     }
   },
   async self(req, res) {
@@ -70,44 +74,38 @@ const userController = {
     // check if is admin
     try {
       if (req.params.privilege === 'admin') {
-        await knex('users')
-          .where('id', req.body.id)
-          .first()
-          .update({ privilege: 2, updated_at: knex.fn.now() });
-
+        await userResource.setPrivilegeById(req.body.id, 2);
         return res.send();
       }
 
       if (req.params.privilege === 'mod') {
-        await knex('users')
-          .where('id', req.body.id)
-          .first()
-          .update({ privilege: 1, updated_at: knex.fn.now() });
-
+        await userResource.setPrivilegeById(req.body.id, 1);
         return res.send();
       }
+      return res.status(400).json({ message: 'Missing parameter' });
     } catch (error) {
-      return res.status(500).json({ message: 'Error on Server', error });
+      return res
+        .status(500)
+        .json({ message: 'Error on Setting Privilege', error });
     }
   },
   async unsetPrivilege(req, res) {
     // check if is admin
     try {
-      await knex('users')
-        .where('id', req.body.id)
-        .first()
-        .update({ privilege: 0, updated_at: knex.fn.now() });
+      await userResource.setPrivilegeById(req.body.id, 0);
 
       return res.send();
     } catch (error) {
-      return res.status(500).json({ message: 'Error on Server', error });
+      return res
+        .status(500)
+        .json({ message: 'Error on Setting Privilege', error });
     }
   },
   async delete(req, res) {
     try {
       // delete user by id
       // cascade configured on migrations
-      await knex('users').del().where({ id: req.user.id });
+      await userResource.deleteCurrentUser(req.user.id);
 
       // return user
       res.json(getUserWithoutPassword(req.user));
---
+++ src/resources/user.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const knex = require('../../database/connection');
+
+module.exports = {
+  getUserById: async (id) => {
+    const [user] = await knex('users').where({ id });
+    return user;
+  },
+  getAllUsers: async () => {
+    const users = await knex('users').orderBy('id');
+    return users;
+  },
+  getGoogleUserProfile: async (tokenId) => {
+    const { data } = await axios.get(
+      `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${tokenId}`
+    );
+    return data;
+  },
+  getUpdatedUser: async ({ name, email, picture }) => {
+    const [user] = await knex('users')
+      .where({ email })
+      .first()
+      .update({ name, picture, updated_at: knex.fn.now() })
+      .returning('*');
+    return user;
+  },
+  getInsertedUser: async ({ name, email, picture }) => {
+    const [user] = await knex('users')
+      .insert({ name, email, picture })
+      .returning('*');
+    return user;
+  },
+  setPrivilegeById: async (id, privilege) => {
+    await knex('users')
+      .where({ id })
+      .first()
+      .update({ privilege, updated_at: knex.fn.now() });
+  },
+  deleteCurrentUser: async (id) => {
+    await knex('users').del().where({ id });
+  }
+};
