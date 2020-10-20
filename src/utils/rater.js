c5142f19432542771e8744f307d304f5ee15dde5
Edmundo Paulino <manyymoore@gmail.com>
2020-10-20 15:34:53 -0300

    Update rateLimit 1 per second => 600 each 10 minutes

--- src/utils/rater.js
+++ src/utils/rater.js
@@ -5,11 +5,9 @@ const rateLimit = require('express-rate-limit');
 // app.set('trust proxy', 1);
 
 const limiter = rateLimit({
-  windowMs: 1000,
-  max: 1,
-  message: {
-    message: 'Too many requests, please try again later.'
-  }
+  windowMs: 10 * 60 * 1000,
+  max: 600,
+  message: { message: 'Too many requests, please try again later.' }
 });
 
 module.exports = { limiter };
