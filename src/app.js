41056954e074cb858beb79dac97ed49832091c73
Edmundo Paulino <manyymoore@gmail.com>
2020-10-21 13:09:24 -0300

    Deactivate limiter

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ const { errors } = require('celebrate');
 const path = require('path');
 const cors = require('cors');
 const routers = require('./routers');
-const { limiter } = require('./utils/rater');
+// const { limiter } = require('./utils/rater');
 
 const app = express();
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
-app.use(limiter);
+// app.use(limiter);
 
 // routers
 app.use(routers.userRouter);
