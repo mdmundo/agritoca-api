899ec6bc190dc719f5694ac5bdd3462d4b4f99de
Edmundo Paulino <manyymoore@gmail.com>
2020-11-19 16:32:32 -0300

    Update Migrations Remove uniqueness from ncm

--- database/migrations/03-create-products.js
+++ database/migrations/03-create-products.js
@@ -1,7 +1,7 @@
 exports.up = (knex) =>
   knex.schema.createTable('products', (table) => {
     table.increments().primary().index();
-    table.string('ncm').unique().index().defaultTo('No ncm provided');
+    table.string('ncm').index().defaultTo('No ncm provided');
     table.string('measure').defaultTo('No measure provided');
     table.binary('picture').defaultTo(null);
     table.string('description').defaultTo('No description provided');
