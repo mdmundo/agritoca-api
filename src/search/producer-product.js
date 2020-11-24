0323c5196fee0529c64ff8f22bb1ffe2578400c3
Edmundo Paulino <manyymoore@gmail.com>
2020-11-24 08:03:01 -0300

    Update search test in production...😱😱😱😨😨😨

--- src/search/producer-product.js
+++ src/search/producer-product.js
@@ -2,6 +2,8 @@ const Fuse = require('fuse.js');
 
 const producerProductSearch = ({ pattern, producerProducts }) => {
   const fuse = new Fuse(producerProducts, {
+    threshold: 0.3,
+    ignoreLocation: true,
     keys: ['ncm', 'description', 'brand', 'keywords', 'info']
   });
   const result = fuse.search(pattern);
