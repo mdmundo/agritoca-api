56c557c49dc489437e0653b34188a40f2a237e0c
Edmundo Paulino <manyymoore@gmail.com>
2020-11-24 08:21:45 -0300

    Update Schema Update basket item. It's a string now.

--- src/schemas/basket.js
+++ src/schemas/basket.js
@@ -3,10 +3,10 @@ const { Joi } = require('celebrate');
 module.exports = {
   update: {
     body: Joi.array().items(
-      Joi.object({
+      Joi.object().keys({
         id: Joi.string().uuid(),
         name: Joi.string().max(255),
-        items: Joi.string(),
+        items: Joi.string().empty(''),
         notes: Joi.string().max(255).empty('')
       })
     )
